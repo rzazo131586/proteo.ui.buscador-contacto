ddad62dacb5e9f4aab35572fc417729d204010e0
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-05 12:55:37 +0100

    Add logica de servicio into Contro

--- src/proteo.ui.buscador-contacto-directive.js
+++ src/proteo.ui.buscador-contacto-directive.js
@@ -2,11 +2,51 @@ angular.module("proteo.ui.buscador-contacto", []).controller("buscadorContactotC
 
 		console.log("Controller buscadorContactotCtrl");
 
+		$scope.filter = {};
+		$scope.contactsFiltered = [];
+
 		$http.get('http://localhost:3000/users').
         success(function(data) {
             $scope.contacts = data;
+            $scope.contactsFiltered = data;
         });
+        
+        $scope.contactReset = function(){
+			$scope.contactsFiltered = $scope.contacts;
+			document.getElementsByName('name').text = "";
+        }
 
+        $scope.contactFiltered = function (){
+        	console.log("Clicked");
+        	$scope.contactsFiltered = [];
+
+        	for(var i = 0; i < $scope.contacts.length ; i++)
+			{	
+				if ($scope.filter.name == undefined && $scope.filter.lastname == undefined && $scope.filter.corporate == undefined){
+					// no data in any input 
+					$scope.contactsFiltered = $scope.contacts;
+					return;
+				}else{
+					// validate its username like to name
+					if( $scope.contacts[i].username.indexOf($scope.filter.name) != -1){
+						console.log(i+" "+$scope.contacts[i].name);
+						$scope.contactsFiltered.push($scope.contacts[i]);
+					}else{
+						// validate if name lide to lastname
+						if( $scope.contacts[i].name.indexOf($scope.filter.lastname) != -1){
+							console.log(i+" "+$scope.contacts[i].name);
+							$scope.contactsFiltered.push($scope.contacts[i]);
+						}else{
+							// validate if company like corporate
+							if( $scope.contacts[i].company.name.indexOf($scope.filter.corporate) != -1){
+								console.log(i+" "+$scope.contacts[i].name);
+								$scope.contactsFiltered.push($scope.contacts[i]);
+							}
+						}
+					}
+				}
+       	 	};
+ 		}
 
 })
 .directive("buscadorContacto", function() {
@@ -29,4 +69,3 @@ angular.module("proteo.ui.buscador-contacto", []).controller("buscadorContactotC
 	}
 
 });
-
