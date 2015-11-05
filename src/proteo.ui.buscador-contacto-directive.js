0abff68e653d7ff302abdfbbd425808df4f08cb5
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-05 11:10:02 +0100

    add directives Buscador & Listado

--- src/proteo.ui.buscador-contacto-directive.js
+++ src/proteo.ui.buscador-contacto-directive.js
@@ -1,6 +1,6 @@
-angular.module("proteo.ui.buscador-contacto", []).controller("buscadorContactotCtrl",  function($scope) {
+angular.module("proteo.ui.buscador-contacto", []).controller("buscadorContactotCtrl",  function($scope, $http) {
 
-		console.log("Controller buscadorContacto");
+		console.log("Controller buscadorContactotCtrl");
 
 		$http.get('http://localhost:3000/users').
         success(function(data) {
@@ -18,61 +18,15 @@ angular.module("proteo.ui.buscador-contacto", []).controller("buscadorContactotC
 		templateUrl: './bower_components/proteo.ui.buscador-contacto/dist/proteo.ui.buscador-contacto.tpl.html'
 	}
 
+})
+.directive("listadoContacto", function() {
+
+	console.log("Directive listadoContacto");
+	return {
+		restrict: "AE",
+		controller : 'buscadorContactotCtrl',
+		templateUrl: './bower_components/proteo.ui.buscador-contacto/dist/proteo.ui.buscador-contacto.tpl.html'
+	}
+
 });
 
-
-/*
-		$scope.information = [
-                { 
-                    "name" : "Karin Bernard",
-                    "lastname" : "1",
-                    "corporate" : "Everis"
-                },
-                { 
-                    "name" : "Noreen Ware",
-                    "lastname" : "2",
-                    "corporate" : "Banco Santander"
-                },
-                { 
-                    "name" : "Elnora Quinn",
-                    "lastname" : "3",
-                    "corporate" : "Everis"
-                },
-                { 
-                    "name" : "Randi Rogers",
-                    "lastname" : "5",
-                    "corporate" : "Everis"
-                },
-                { 
-                    "name" : "Bonner Mosley",
-                    "lastname" : "6",
-                    "corporate" : "Proteo"
-                },
-                { 
-                    "name" : "Marianne Ellison",
-                    "lastname" : "7",
-                    "corporate" : "Proteo"
-                },
-                { 
-                    "name" : "Le Hudson",
-                    "lastname" : "8",
-                    "corporate" : "Everis"
-                },
-                { 
-                    "name" : "Minerva Alford",
-                    "lastname" : "9",
-                    "corporate" : "Banco Santander"
-                },
-                { 
-                    "name" : "Lamb Baird",
-                    "lastname" : "10",
-                    "corporate" : "Banco Santander"
-                },
-                { 
-                    "name" : "Davenport Rush",
-                    "lastname" : "100",
-                    "corporate" : "Everis"
-                }
-            ];
-
- */
