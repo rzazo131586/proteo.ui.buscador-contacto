5cab9c38f986a505e9b746ea47e3a5465aa14952
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-05 11:02:09 +0100

    add service REST Json User

--- dist/proteo.ui.buscador-contacto.js
+++ dist/proteo.ui.buscador-contacto.js
@@ -3,6 +3,12 @@ angular.module("proteo.ui.buscador-contacto", []).controller("buscadorContactotC
 
 		console.log("Controller buscadorContacto");
 
+		$http.get('http://localhost:3000/users').
+        success(function(data) {
+            $scope.contacts = data;
+        });
+
+
 })
 .directive("buscadorContacto", function() {
 
@@ -14,4 +20,61 @@ angular.module("proteo.ui.buscador-contacto", []).controller("buscadorContactotC
 	}
 
 });
+
+
+/*
+		$scope.information = [
+                { 
+                    "name" : "Karin Bernard",
+                    "lastname" : "1",
+                    "corporate" : "Everis"
+                },
+                { 
+                    "name" : "Noreen Ware",
+                    "lastname" : "2",
+                    "corporate" : "Banco Santander"
+                },
+                { 
+                    "name" : "Elnora Quinn",
+                    "lastname" : "3",
+                    "corporate" : "Everis"
+                },
+                { 
+                    "name" : "Randi Rogers",
+                    "lastname" : "5",
+                    "corporate" : "Everis"
+                },
+                { 
+                    "name" : "Bonner Mosley",
+                    "lastname" : "6",
+                    "corporate" : "Proteo"
+                },
+                { 
+                    "name" : "Marianne Ellison",
+                    "lastname" : "7",
+                    "corporate" : "Proteo"
+                },
+                { 
+                    "name" : "Le Hudson",
+                    "lastname" : "8",
+                    "corporate" : "Everis"
+                },
+                { 
+                    "name" : "Minerva Alford",
+                    "lastname" : "9",
+                    "corporate" : "Banco Santander"
+                },
+                { 
+                    "name" : "Lamb Baird",
+                    "lastname" : "10",
+                    "corporate" : "Banco Santander"
+                },
+                { 
+                    "name" : "Davenport Rush",
+                    "lastname" : "100",
+                    "corporate" : "Everis"
+                }
+            ];
+
+ */
 /* Todo Filter */
