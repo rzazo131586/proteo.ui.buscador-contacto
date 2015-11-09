b08f880ab09420120b0f7bd174c128287c30517e
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 16:50:09 +0100

    cambio de logica > llamando a dos componentes (filtro+visor)

--- src/proteo.ui.buscador-contacto-module.js
+++ src/proteo.ui.buscador-contacto-module.js
@@ -1,1 +1,11 @@
-/* Todo Module */
+/* Buscador-Contacto Module */
+(function () {
+
+    'use strict';	
+
+	angular.module('proteo.ui.buscador-contacto',['pascalprecht.translate'])
+		.config(['$translateProvider', function ($translateProvider) {
+			// custom code 
+	}]);
+}());
+/* END Buscador-Contacto Module */
