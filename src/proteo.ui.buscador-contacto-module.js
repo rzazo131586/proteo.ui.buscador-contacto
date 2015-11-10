9c553415982c7e292cd5a577e6a37823df61182a
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-10 13:08:43 +0100

    add dependencias modulos simples

--- src/proteo.ui.buscador-contacto-module.js
+++ src/proteo.ui.buscador-contacto-module.js
@@ -3,7 +3,7 @@
 
     'use strict';	
 
-	angular.module('proteo.ui.buscador-contacto',['pascalprecht.translate'])
+	angular.module('proteo.ui.buscador-contacto',['pascalprecht.translate', 'proteo.ui.buscador-contacto.filtro', 'proteo.ui.buscador-contacto.visor'])
 		.config(['$translateProvider', function ($translateProvider) {
 			// custom code 
 	}]);
