79517ff5bd19b5b3e8c3b38acfb9f9f475a9a702
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-10 09:58:10 +0100

    DIST

--- dist/proteo.ui.buscador-contacto.js
+++ dist/proteo.ui.buscador-contacto.js
@@ -1,3 +1,54 @@
+/* Buscador-Contacto Module */
+(function () {
+
+    'use strict';	
+
+	angular.module('proteo.ui.buscador-contacto',['pascalprecht.translate'])
+		.config(['$translateProvider', function ($translateProvider) {
+			// custom code 
+	}]);
+}());
+/* END Buscador-Contacto Module */
+
+/* Buscador-Contacto Controller */
+(function () {
+    'use strict';
+
+	angular.module('proteo.ui.buscador-contacto').controller('buscadorContactoCtrl', function($scope, $http) {
+		// Code fo Controller
+		/*            $scope.contacstList ={};*/
+            $scope.contactsToSearch = function(valueDirective){
+                //console.log("ValueDirective >> "+JSON.stringify(valueDirective));
+                $scope.contacstList = valueDirective;
+                $scope.contacstToShow = $scope.contacstList;
+                //console.log("$scope.contacstToShow >> "+JSON.stringify($scope.contacstList));
+            };
+            
+/*            $scope.$watch('contacstList', function(){
+                $scope.contacstToShow = $scope.contacstList;
+            });*/
+	});
+
+}());
+/* END Buscador-Contacto Controller */
+
+/* Buscador-Contacto Directive */
+(function () {
+    'use strict';
+
+	angular.module('proteo.ui.buscador-contacto').directive('buscadorContacto',function() {
+	  return {
+		restrict	: 	"E",
+	    controller 	: 	'buscadorContactoCtrl',
+	    templateUrl : 	'./bower_components/proteo.ui.buscador-contacto/dist/proteo.ui.buscador-contacto.tpl.html',
+	  };
+	});
+}());
+/* END Buscador-Contacto Directive */
+
+/* Buscador-Contacto Filter */
+/* END Buscador-Contacto Filter */
+
 /* Buscador-Contacto.FILTRO MODULE */
 (function () {
 
@@ -159,53 +210,3 @@
 
 }());
 /* END Buscador-Contacto.VISOR SERVICE */
-/* Buscador-Contacto Controller */
-(function () {
-    'use strict';
-
-	angular.module('proteo.ui.buscador-contacto').controller('buscadorContactoCtrl', function($scope, $http) {
-		// Code fo Controller
-		/*            $scope.contacstList ={};*/
-            $scope.contactsToSearch = function(valueDirective){
-                //console.log("ValueDirective >> "+JSON.stringify(valueDirective));
-                $scope.contacstList = valueDirective;
-                $scope.contacstToShow = $scope.contacstList;
-                //console.log("$scope.contacstToShow >> "+JSON.stringify($scope.contacstList));
-            };
-            
-/*            $scope.$watch('contacstList', function(){
-                $scope.contacstToShow = $scope.contacstList;
-            });*/
-	});
-
-}());
-/* END Buscador-Contacto Controller */
-
-/* Buscador-Contacto Directive */
-(function () {
-    'use strict';
-
-	angular.module('proteo.ui.buscador-contacto').directive('buscadorContacto',function() {
-	  return {
-		restrict	: 	"E",
-	    controller 	: 	'buscadorContactoCtrl',
-	    templateUrl : 	'./bower_components/proteo.ui.buscador-contacto/dist/proteo.ui.buscador-contacto.tpl.html',
-	  };
-	});
-}());
-/* END Buscador-Contacto Directive */
-
-/* Buscador-Contacto Filter */
-/* END Buscador-Contacto Filter */
-
-/* Buscador-Contacto Module */
-(function () {
-
-    'use strict';	
-
-	angular.module('proteo.ui.buscador-contacto',['pascalprecht.translate'])
-		.config(['$translateProvider', function ($translateProvider) {
-			// custom code 
-	}]);
-}());
-/* END Buscador-Contacto Module */
